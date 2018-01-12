36b498701c64cd0e03d362f54e94a6f701584ae9
Selvaganesh G <selvaganesh_g@photonint.com>
2018-01-12 16:40:03 +0530

    First Commit

---
+++ src/main/java/com/ai/Anetwork/AnetworkApplication.java
@@ -0,0 +1,12 @@
+package com.ai.Anetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnetworkApplication.class, args);
+	}
+}
